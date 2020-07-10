7e757aa34b2e5ca8ebbfc069204e7a77d19a119f
Kirk Veitch <kirk.a.veitch@gmail.com>
2020-07-10 01:19:04 -0600

    Move functionality to function  called with setInterval

--- darkClock.js
+++ darkClock.js
@@ -1,14 +1,15 @@
-const hr = document.querySelector('#hr');
-const min = document.querySelector('#min');
-const sec = document.querySelector('#sec');
+const hr = document.querySelector('.hr');
+const min = document.querySelector('.min');
+const sec = document.querySelector('.sec');
 
-setInterval(() => {
-let deg = 6;
-let day = new Date();
-let hourPos = day.getHours() * 30;
-let minPos = day.getMinutes() * deg;
-let secPos = day.getSeconds() * deg;
+setInterval(setTime,1000);
+
+function setTime(){
+  let day = new Date();
+  let hourPos = day.getHours() * 30;
+  let minPos = day.getMinutes() * 6;
+  let secPos = day.getSeconds() * 6;
   hr.style.transform = `rotate(${hourPos + minPos / 12}deg`;
   min.style.transform = `rotate(${minPos}deg`;
   sec.style.transform = `rotate(${secPos}deg`;
-});
+}
